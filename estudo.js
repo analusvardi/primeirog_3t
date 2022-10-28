605d8755137fca0b48db0f5de2437af6d02d6b89
lsvdz <110683775+analusvardi@users.noreply.github.com>
2022-10-28 12:08:20 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -61,4 +61,11 @@ function total(){
           let r = Number(val1) * Number(p);
           document.getElementById("resultado").innerHTML =r;
           }
-    
+          function porcentagem(){
+              let val1 = document.getElementById("v1").value;
+              let val2 = document.getElementById("v2").value;
+              let c = 100
+              let p = Number(val2)  / Number(c)
+              let r = Number(val1) * Number(p);
+              document.getElementById("resultado").innerHTML =r;
+              }
